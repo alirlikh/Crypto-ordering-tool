709562578128435c7983585f054e88fedde43350
Alireza Jalili <alirlikh@gmail.com>
2024-08-17 21:33:16 +0330

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        sm: "320px",
+        md: "768px",
+        lg: "976px"
+      }
+    }
+  },
+  plugins: []
+}
