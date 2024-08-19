6aea902187f117d88385d096ed3a25cb804d92a4
Alireza Jalili <alirlikh@gmail.com>
2024-08-19 12:55:57 +0330

    feat: config orders api

--- src/services/orders/orders.api.js
+++ src/services/orders/orders.api.js
@@ -1,5 +1,11 @@
 import request from "../api"
 
-export const getOrder = (market_id) => {
+export const getSellOrder = (market_id) => {
   return request.get(`v2/mth/actives/${market_id}/?type=sell`)
+}
+export const getBuyOrder = (market_id) => {
+  return request.get(`v2/mth/actives/${market_id}/?type=buy`)
+}
+export const getMatches = (market_id) => {
+  return request.get(`v1/mth/matches/${market_id}/`)
 }
