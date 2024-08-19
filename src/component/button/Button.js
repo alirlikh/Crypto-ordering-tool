1f592562536ca82dcfc60977b4e3bd4235581ffb
Alireza Jalili <alirlikh@gmail.com>
2024-08-19 22:00:09 +0330

    feat: add button component

---
+++ src/component/button/Button.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Button = ({ children, onClick, loading = false, disabled = false }) => {
+  return (
+    <button
+      loading={loading}
+      disabled={disabled}
+      onClick={onClick}
+      className="bg-btn-green transition-all duration-150 hover:brightness-[0.8] text-black font-medium py-3 px-6 rounded w-24 outline-0"
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
