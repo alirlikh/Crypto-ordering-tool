ceb6a962f1a7aad9a4cb70c9a3f04f622b802e98
Alireza Jalili <alirlikh@gmail.com>
2024-08-20 19:04:09 +0330

    feat: add loader

---
+++ src/component/loader/Loader.js
@@ -0,0 +1,11 @@
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="dot1"></div>
+      <div className="dot2"></div>
+      <div className="dot3"></div>
+    </div>
+  )
+}
+
+export default Loader
