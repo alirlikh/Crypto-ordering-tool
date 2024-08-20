16f26b310f177bd5dfe341381cab3d95f6cfc1ee
Alireza Jalili <alirlikh@gmail.com>
2024-08-20 23:53:22 +0330

    feat: add footer

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -5,17 +5,19 @@ import { Moon } from "../assets/icon/Moon"
 const MainLayout = ({ children }) => {
   const { theme, toggleTheme } = useContext(themeContext)
   return (
-    <div className="p-2">
+    <div className="">
       <header className=" h-20">
         <div className="relative left-0 top-0 ">
-          <button className="fixed left-0 mx-10 my-5 border-none" onClick={toggleTheme}>
+          <button className="absolute left-0 mx-10 my-5 border-none" onClick={toggleTheme}>
             {theme === "dark" ? <Sun /> : <Moon />}
           </button>
         </div>
       </header>
       <main>{children}</main>
-      <footer>
-        <div>Made for Bitpin</div>
+      <footer className="w-full">
+        <div className="bg-probe_chart_card w-full h-6 text-regular_text text-center mt-6 relative bottom-0">
+          Made for Bitpin
+        </div>
       </footer>
     </div>
   )
