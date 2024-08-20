cf9a626a20b596373693ff368c2ad4eff2498551
Alireza Jalili <alirlikh@gmail.com>
2024-08-20 23:01:25 +0330

    feat: add new props to button

--- src/component/button/Button.js
+++ src/component/button/Button.js
@@ -1,12 +1,11 @@
 import React from "react"
 
-const Button = ({ children, onClick, loading = false, disabled = false }) => {
+const Button = ({ children, onClick, disabled = false, className }) => {
   return (
     <button
-      loading={loading}
       disabled={disabled}
       onClick={onClick}
-      className="bg-btn-green transition-all duration-150 hover:brightness-[0.8] text-black font-medium py-3 px-6 rounded w-24 outline-0 text-base"
+      className={`bg-btn-green transition-all duration-150 hover:brightness-[0.8] text-black font-medium py-3 px-6 rounded w-24 outline-0 text-base ${className}`}
     >
       {children}
     </button>
