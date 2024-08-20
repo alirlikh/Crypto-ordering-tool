a28796b1233f7f15638db5c54fcfb4a97d2616a5
Alireza Jalili <alirlikh@gmail.com>
2024-08-20 23:51:02 +0330

    fix: loader in routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 import React, { lazy, Suspense } from "react"
 import { Routes, Route } from "react-router-dom"
 import NotFound from "./component/not-found/NotFound"
+import Loader from "./component/loader/Loader"
 
 export const renderRoutes = (routes) => {
   return (
-    <Suspense fallback={<div>loading...</div>}>
+    <Suspense fallback={<Loader />}>
       <Routes>
         {routes.map((route, index) => (
           <Route key={index} path={route.path} element={route.element} />
@@ -20,7 +21,7 @@ const Orders = React.lazy(() => import("./pages/orders/Orders"))
 const routes = [
   { path: "/", element: <Markets /> },
   { path: "/orders/:currencyId", element: <Orders /> },
-  { path: "*", element: <NotFound/> }
+  { path: "*", element: <NotFound /> }
 ]
 
 export default routes
