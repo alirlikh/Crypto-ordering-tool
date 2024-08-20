7a88e62c551457d641a6169a2bf0e9d1ef18f5be
Alireza Jalili <alirlikh@gmail.com>
2024-08-21 02:04:43 +0330

    fix: href

--- src/component/card/Card.js
+++ src/component/card/Card.js
@@ -1,6 +1,7 @@
 import React from "react"
 import LinkButton from "../link-button/LinkButton"
 import Decimal from "decimal.js"
+import { NavLink } from "react-router-dom"
 
 const Card = ({ tableData }) => {
   const results = tableData
@@ -37,58 +38,67 @@ const Card = ({ tableData }) => {
         </div>
         {results.length > 0 ? (
           results.map((item, index) => (
-            <div
-              key={index}
-              className="flex flex-row w-full justify-between items-center px-4 *:ltr-grid *:text-center  hover:bg-hover-col "
+            <NavLink
+              to={`orders/${item.id}`}
+              className={"flex flex-row w-full justify-between items-center  "}
+              onClick={() => setLocalData(item)}
             >
-              <div className="flex flex-row  md:w-1/5  w-2/5">
-                <div className=" shrink-0 relative">
-                  <img
-                    src={item.currency1.image ? item.currency1.image : ""}
-                    alt={item.currency1.title_fa}
-                    className="w-10"
-                  />
-                  <img
-                    src={item.currency2.image ? item.currency2.image : ""}
-                    alt={item.currency2.title_fa}
-                    className="absolute bottom-2 right-0"
-                    width={20}
+              <div
+                key={index}
+                className="flex flex-row w-full justify-between items-center px-4 *:ltr-grid *:text-center  hover:bg-hover-col "
+              >
+                <div className="flex flex-row  md:w-1/5  w-2/5">
+                  <div className=" shrink-0 relative">
+                    <img
+                      src={item.currency1.image ? item.currency1.image : ""}
+                      alt={item.currency1.title_fa}
+                      className="w-10"
+                    />
+                    <img
+                      src={item.currency2.image ? item.currency2.image : ""}
+                      alt={item.currency2.title_fa}
+                      className="absolute bottom-2 right-0"
+                      width={20}
+                    />
+                  </div>
+                  <div className="flex flex-col mx-2 p-2">
+                    <span className="text-bold_text text-sm font-extrabold">
+                      {item.currency1.title_fa}
+                    </span>
+                    <span>
+                      {item.currency1.code}/{item.currency2.code}
+                    </span>
+                  </div>
+                </div>
+                <div className="tab2  ltr-grid md:w-1/5  w-1/3">
+                  <span>{item.price ? new Decimal(item.price).toFixed().toString() : "-"}</span>
+                </div>
+                <div className="tab3 md:w-1/6 w-1/4 ltr-grid">
+                  <span
+                    className={` px-6 py-4 text-bold_text my-2 ltr-grid ${
+                      item.price_info.change > 0 ? "text-green_text" : "text-red_text"
+                    }`}
+                  >
+                    {item.price_info.change
+                      ? `%${new Decimal(item.price_info.change).toFixed(2).toString()}`
+                      : "-"}
+                  </span>
+                </div>
+                {/* <div className="tab4  hidden md:block  md:w-1/5">
+              <span>نمودار</span>
+            </div> */}
+                <div className="tab5  md:w-1/5 px-6 py-4 text-bold_text text-right my-2 hidden md:block">
+                  <LinkButton
+                    href={`orders/${item.id}`}
+                    name={"مشاهده"}
+                    onClick={() => setLocalData(item)}
+                    className={
+                      "bg-btn-green transition-all duration-150 hover:brightness-[0.8] text-black font-medium py-3 px-6 rounded"
+                    }
                   />
                 </div>
-                <div className="flex flex-col mx-2 p-2">
-                  <span className="text-bold_text text-sm font-extrabold">
-                    {item.currency1.title_fa}
-                  </span>
-                  <span>
-                    {item.currency1.code}/{item.currency2.code}
-                  </span>
-                </div>
               </div>
-              <div className="tab2  ltr-grid md:w-1/5  w-1/3">
-                <span>{item.price ? new Decimal(item.price).toFixed().toString() : "-"}</span>
-              </div>
-              <div className="tab3 md:w-1/6 w-1/4 ltr-grid">
-                <span
-                  className={` px-6 py-4 text-bold_text my-2 ltr-grid ${
-                    item.price_info.change > 0 ? "text-green_text" : "text-red_text"
-                  }`}
-                >
-                  {item.price_info.change
-                    ? `%${new Decimal(item.price_info.change).toFixed(2).toString()}`
-                    : "-"}
-                </span>
-              </div>
-              {/* <div className="tab4  hidden md:block  md:w-1/5">
-                <span>نمودار</span>
-              </div> */}
-              <div className="tab5  md:w-1/5 px-6 py-4 text-bold_text text-right my-2 hidden md:block">
-                <LinkButton
-                  href={`orders/${item.id}`}
-                  name={"مشاهده"}
-                  onClick={() => setLocalData(item)}
-                />
-              </div>
-            </div>
+            </NavLink>
           ))
         ) : (
           <div>
